908eebde7ba711d0b5eb07fa230c69919590e7da
Nepo Rojas • GECI <braulio.rojas@islas.org.mx>
2025-01-09 14:36:49 -0800

    🔧🤑 Use cheaper machine

--- src/webserver.tf
+++ src/webserver.tf
@@ -41,7 +41,7 @@ resource "azurerm_linux_virtual_machine" "webserver" {
   name                = "webserver"
   resource_group_name = azurerm_resource_group.webserver.name
   location            = azurerm_resource_group.webserver.location
-  size                = "Standard_D2as_v4"
+  size                = "Standard_B1s"
   admin_username      = "ciencia_datos"
   network_interface_ids = [
     azurerm_network_interface.webserver.id,
