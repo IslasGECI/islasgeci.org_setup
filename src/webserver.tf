16eb71e4aec1cb45a51e541965a9cd0cf5f85e65
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2026-05-28 11:37:22 -0700

    🔒 Create NSG with inbound rules for published ports
    Add azurerm_network_security_group with inline security_rules for
    SSH (22), HTTP (80), and application ports (100, 200, 300, 500).
    Associate the NSG with the network interface at the NIC level.

--- src/webserver.tf
+++ src/webserver.tf
@@ -24,10 +24,89 @@ resource "azurerm_public_ip" "webserver" {
   allocation_method   = "Static"
 }
 
-resource "azurerm_network_interface" "webserver" {
-  name                = "webserver-nic"
+resource "azurerm_network_security_group" "webserver" {
+  name                = "webserver-nsg"
   location            = azurerm_resource_group.webserver.location
   resource_group_name = azurerm_resource_group.webserver.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 90
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowPort100"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "100"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowPort200"
+    priority                   = 130
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "200"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowPort300"
+    priority                   = 140
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "300"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowPort500"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "500"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "webserver" {
+  name                      = "webserver-nic"
+  location                  = azurerm_resource_group.webserver.location
+  resource_group_name       = azurerm_resource_group.webserver.name
+  network_security_group_id = azurerm_network_security_group.webserver.id
 
   ip_configuration {
     name                          = "internal"
